f220f43a3407f0b334afa1fd596ea00ca222832f
Jorge Rojas <jorgearojas25@gmail.com>
2020-02-13 19:43:00 -0500

    Proceso de bd

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const Usuarios = [];
+const Productos = [];
+const Pedidos = [];
+
 const Ejecutar = () => {
-    
+
 }
+
+/*
+Menu 
+Registro de users
+Lista con usuarios
+Registrar Productos
+ListarProductos
+
+
+
+*/
